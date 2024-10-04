d02d82b101503ceda6fb878177981ceef24d1f00
Adam Kecskes <adam@kecskes.net>
2024-10-04 15:48:40 -0500

    add initial command line option choice(s)

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,29 @@
 --Copyright Laurence Emms 2018
 --Text adventure executable
 
-import System.IO
+-- import System.IO
 
-import NarrativeGraph hiding (endScenes)
-import PrintUtils
-import TextAdventureCore
-import TextReflow
+import CmdOptions
+-- import NarrativeGraph hiding (endScenes)
+-- import PrintUtils
+-- import TextAdventureCore
+-- import TextReflow
 
-import DummyAdventure
+-- import DummyAdventure
+
 
 main :: IO ()
-main = printIntro >>
-       reflowPutStr allDelimiters allColumnWidth gameIntro >>
-       putStr "\n" >>
-       printHelp >>
-       putStr "\n" >>
-       putStr "\n" >>
-       putStr "\n" >>
-       hFlush stdout >>
-       adventure (makeNarrativeGraph adventureScenes endScenes defaultScene) (Just (startScene, startInventory, startFlags)) >>
-       return ()
-           where (adventureScenes, endScenes) = allScenes
+main = greet =<< parse
+
+
+-- main = printIntro >>
+--        reflowPutStr allDelimiters allColumnWidth gameIntro >>
+--        putStr "\n" >>
+--        printHelp >>
+--        putStr "\n" >>
+--        putStr "\n" >>
+--        putStr "\n" >>
+--        hFlush stdout >>
+--        adventure (makeNarrativeGraph adventureScenes endScenes defaultScene) (Just (startScene, startInventory, startFlags)) >>
+--        return ()
+          --  where (adventureScenes, endScenes) = allScenes
--- src/CmdOptions.hs
+++ src/CmdOptions.hs
@@ -1,14 +1,29 @@
-module CmdOptions where
+module CmdOptions (parse, greet) where
 
 import Options.Applicative
 
-data Adventure = Adventure
-  { adventure :: String }
+data AdventureOptions = AdventureOptions String
+  -- { hello      :: String
+  -- , quiet      :: Bool
+  -- , enthusiasm :: Int }
 
-adventureOpts :: Parser Adventure
-adventureOpts = Adventure
+adventure :: Parser AdventureOptions
+adventure = AdventureOptions
       <$> strOption
           ( long "adventure"
-         <> short 'a'
-         <> metavar "ADVENTURE"
-         <> help "Name of Adventure to choose" )
+          <> short 'a'
+          <> metavar "NAME"
+          <> help "Name of adventure to load." )
+
+greet :: AdventureOptions -> IO ()
+greet (AdventureOptions a ) = putStrLn $ "You chose: '" ++ a ++ "'." 
+
+parse :: IO AdventureOptions
+parse = execParser opts
+
+opts :: ParserInfo AdventureOptions
+opts = info (adventure <**> helper)
+      ( fullDesc
+      <> progDesc "Run the named text adventure."
+      <> header "Haskell Adventure - a journey into fun!" )
+
