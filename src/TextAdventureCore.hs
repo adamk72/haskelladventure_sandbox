7a8a3208e323129fed20be70202cb02835624d0a
Adam Kecskes <adam@kecskes.net>
2024-10-04 15:00:39 -0500

    refactor to pull more out of Main

---
+++ src/TextAdventureCore.hs
@@ -0,0 +1,33 @@
+module TextAdventureCore where
+
+import System.IO
+
+import NarrativeGraph
+import NaturalLanguageParser
+import PrintUtils
+import TextReflow
+
+import DummyAdventure
+
+doAdventureLoop :: NarrativeGraph -> SceneKey -> Inventory -> Flags -> Maybe [Sentence] -> IO (Maybe (SceneKey, Inventory, Flags))
+doAdventureLoop _ _ _ _ Nothing = return Nothing -- End state of the game
+doAdventureLoop narrativeGraph sceneKey inventory flags (Just []) = adventure narrativeGraph (Just (sceneKey, inventory, flags)) --Failed to parse any sentences
+doAdventureLoop narrativeGraph sceneKey inventory flags (Just sentences) = performInteraction allDelimiters allColumnWidth narrativeGraph sceneKey inventory flags sentences >>=
+    adventure narrativeGraph --Perform the adventure loop
+
+updateAdventure :: NarrativeGraph -> Maybe (SceneKey, Inventory, Flags) -> IO (Maybe (SceneKey, Inventory, Flags))
+updateAdventure _ Nothing = return Nothing
+updateAdventure narrativeGraph (Just (sceneKey, inventory, flags))
+    = putStr "\n> " >>
+      hFlush stdout >>
+      printInvalidInteractions narrativeGraph sceneKey >>
+      getLine >>=
+      parseInput inventory flags >>=
+      (\state -> putStr "\n" >> hFlush stdout >> return state) >>=
+      doAdventureLoop narrativeGraph sceneKey inventory flags
+
+adventure :: NarrativeGraph -> Maybe (SceneKey, Inventory, Flags) -> IO (Maybe (SceneKey, Inventory, Flags))
+adventure _ Nothing = reflowPutStr allDelimiters allColumnWidth "Game over. Thanks for playing!" >> hFlush stdout >> return Nothing
+adventure narrativeGraph (Just (sceneKey, inventory, flags))
+    = printSceneDescription allDelimiters allColumnWidth narrativeGraph (Just (sceneKey, inventory, flags)) >>=
+      updateAdventure narrativeGraph
