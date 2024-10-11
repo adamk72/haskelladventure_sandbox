0a2e5ccb8f0fa60746826dd2ac1f44c48a3a122d
Adam Kecskes <adam@kecskes.net>
2024-10-10 23:17:26 -0500

    get game to run each game based on command line args

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ s
 
 - [x] Pull list of adventures from a text file and initially display on screen.
 - [x] Update help text with list of adventures so that --help shows them.
-- [] Allow user to input one of those adventure as a command line argument and then display content from a second file associated with that name.
+- [x] Allow user to input one of those adventure as a command line argument and then display content from a second file associated with that name.
 - [] (Low) Add a more complete description of the adventures in the help.
--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,25 @@
 --TextAdventure.hs
 --Copyright Laurence Emms 2018
 --Text adventure executable
-{-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# LANGUAGE LambdaCase #-}
 
-import           System.IO
+import           System.Environment as E (getArgs)
+import           System.IO          (hFlush, stdout)
 
--- import CmdOptions
-import           CmdOptions         (AdventureOptions (AdventureOptions), parse)
-import           DummyAdventure     (allScenes, defaultScene, gameIntro,
-                                     startFlags, startInventory, startScene)
-import           NarrativeGraph     (makeNarrativeGraph)
+import           CmdOptions         (parse)
+import           Data.Map           (Map)
+import qualified DummyAdventure     as Dummy (allScenes, defaultScene,
+                                              gameIntro, startFlags,
+                                              startInventory, startScene)
+import           NarrativeGraph     (Flags, Inventory, Scene, SceneKey,
+                                     makeNarrativeGraph)
+import qualified NightmareAdventure as Nightmare (allScenes, defaultScene,
+                                                  gameIntro, startFlags,
+                                                  startInventory, startScene)
 import           PrintUtils         (allColumnWidth, allDelimiters, printHelp,
                                      printIntro)
 import           TextAdventureCore  (adventure)
 import           TextReflow         (reflowPutStr)
-
-import           System.Environment as E
-
 
 main :: IO ()
 main =
@@ -25,15 +27,37 @@ main =
     E.getArgs >>= \case
         ["-a", "Dummy Adventure"]       -> runDummy
         ["-a", "Dummy"]                 -> runDummy
-        ["-a", "Nightmare Adventure"]   -> runDummy
-        ["-a", "Nightmare"]             -> runDummy
+        ["-a", "Nightmare Adventure"]   -> runNightmare
+        ["-a", "Nightmare"]             -> runNightmare
         _                               -> displayHelp
 
 displayHelp :: IO ()
 displayHelp = readFile "stories.txt" >>= parse >>= mempty
 
 runDummy :: IO ()
-runDummy =
+runDummy = runGame  Dummy.gameIntro
+                    Dummy.defaultScene
+                    Dummy.startScene
+                    Dummy.startInventory
+                    Dummy.startFlags
+                    Dummy.allScenes
+
+runNightmare :: IO ()
+runNightmare = runGame  Nightmare.gameIntro
+                        Nightmare.defaultScene
+                        Nightmare.startScene
+                        Nightmare.startInventory
+                        Nightmare.startFlags
+                        Nightmare.allScenes
+
+runGame :: String ->
+    NarrativeGraph.Scene ->
+    String ->
+    NarrativeGraph.Inventory ->
+    NarrativeGraph.Flags ->
+    (Data.Map.Map NarrativeGraph.SceneKey NarrativeGraph.Scene, [NarrativeGraph.SceneKey]) ->
+    IO ()
+runGame gameIntro defaultScene startScene startInventory startFlags allScenes =
     printIntro >>
     reflowPutStr allDelimiters allColumnWidth gameIntro >>
     putStr "\n" >>
