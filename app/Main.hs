97ec2d6e7cb424608538f4e96f0bed4a5797e6d8
Adam Kecskes <adam@kecskes.net>
2024-10-13 14:03:55 -0500

    wip: get pizza example working including concatenating Text

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,55 @@
 --TextAdventure.hs
 --Copyright Laurence Emms 2018
 --Text adventure executable
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import           CmdOptions           (parse)
+
+import           CmdOptions           as Cmd (parse)
+import           Control.Monad        ()
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as B
 import           Data.Map             (Map)
-import           NarrativeGraph       (Flags, Inventory, Scene, SceneKey,
-                                       makeNarrativeGraph)
-import           PrintUtils           (printHelp, printIntro)
-import           System.Environment   as E (getArgs)
-import           System.IO            (hFlush, stdout)
-import           TextAdventureCore    (adventure)
-import           TextReflow           (reflowPutStr)
-
+import           Data.Text            as T (Text, concat)
 import qualified DummyAdventure       as Dummy (allPrepositions, allScenes,
                                                 allTokens, allVerbs,
                                                 defaultScene, gameIntro,
                                                 startFlags, startInventory,
                                                 startScene)
+import           GHC.Generics
+import           NarrativeGraph       (Flags, Inventory, Scene, SceneKey,
+                                       makeNarrativeGraph)
 import           NaturalLanguageLexer (Prepositions, Tokens, Verbs)
 import qualified NightmareAdventure   as Nightmare (allPrepositions, allScenes,
                                                     allTokens, allVerbs,
                                                     defaultScene, gameIntro,
                                                     startFlags, startInventory,
                                                     startScene)
+import           PrintUtils           (printHelp, printIntro)
+import           System.Environment   as E (getArgs)
+import           System.IO            (hFlush, stdout)
+import           TextAdventureCore    (adventure)
+import           TextReflow           (reflowPutStr)
+
+data Person =
+  Person { firstName  :: !T.Text
+         , lastName   :: !T.Text
+         , age        :: Int
+         , likesPizza :: Bool
+           } deriving (Show,Generic)
+
+instance FromJSON Person
+instance ToJSON Person
+
+jsonFile :: FilePath
+jsonFile = "stories/pizza.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+getStories :: IO String
+getStories = readFile "stories.txt"
 
 main :: IO ()
 main =
@@ -36,7 +62,12 @@ main =
         _                               -> displayHelp
 
 displayHelp :: IO ()
-displayHelp = readFile "stories.txt" >>= parse >>= mempty
+-- displayHelp = getStories >>= Cmd.parse >>= mempty
+displayHelp = do
+ d <- (eitherDecode <$> getJSON) :: IO (Either String [Person])
+ case d of
+  Left err -> putStrLn err
+  Right ps -> print (map (\p -> T.concat [firstName p, " ", lastName p]) ps)
 
 runDummy :: IO ()
 runDummy = runGame Dummy.allVerbs
