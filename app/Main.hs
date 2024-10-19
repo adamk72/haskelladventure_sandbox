3a628e6f92f0f463a4c6bcd7f842a853cbe6e999
Adam Kecskes <adam@kecskes.net>
2024-10-19 10:52:10 -0500

    wip: get parser to pull from multiple files

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,11 @@
 
 
 import           CmdOptions           as Cmd (parse)
-import           Data.Aeson           (FromJSON, ToJSON, eitherDecode)
+import           Control.Monad        (forM_)
+import           Data.Aeson           (FromJSON, Result (..), ToJSON,
+                                       Value (..), decode, eitherDecode,
+                                       fromJSON, (.:))
+import           Data.Aeson.Types     (Parser, parseMaybe)
 import qualified Data.ByteString.Lazy as B
 import           Data.Map             (Map)
 import           Data.Text            as T (Text, concat, intercalate, unpack)
@@ -32,6 +36,7 @@ import qualified NightmareAdventure   as Nightmare (allPrepositions, allScenes,
 import           PrintUtils           (printHelp, printIntro)
 import           System.Directory
 import           System.Environment   as E (getArgs)
+import           System.FilePath      (takeExtension, (</>))
 import           System.IO            (hFlush, stdout)
 import           TextAdventureCore    (adventure)
 import           TextReflow           (reflowPutStr)
@@ -46,7 +51,7 @@ instance FromJSON AdventureDetail
 instance ToJSON AdventureDetail
 
 jsonFile :: FilePath
-jsonFile = "stories/adventures.json"
+jsonFile = "stories/DummyAdventure.json"
 
 storyDirectory :: FilePath
 storyDirectory = "stories"
@@ -56,16 +61,39 @@ getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
 main :: IO ()
-main =
+main = runMainTest
     -- E.getArgs >>= print >>
     --FilePath -> IO [FilePath] >>= m [String]
-    listDirectory storyDirectory >>= mapM_ putStrLn
+    -- listDirectory storyDirectory >>= mapM_ print >>
     -- E.getArgs >>= \case
     --     ["-a", "Dummy Adventure"]       -> runDummy
     --     ["-a", "Dummy"]                 -> runDummy
     --     ["-a", "Nightmare Adventure"]   -> runNightmare
     --     ["-a", "Nightmare"]             -> runNightmare
     --     _                               -> displayHelp
+
+runMainTest :: IO ()
+runMainTest = do
+    files <- listDirectory storyDirectory
+    let jsonFiles = filter (\f -> takeExtension f == ".json") files
+
+    putStrLn "Titles from JSON files:"
+    forM_ jsonFiles $ \file -> do
+        let fullPath = storyDirectory </> file
+        content <- B.readFile fullPath
+        case decode content of
+            Nothing -> putStrLn $ "Error parsing JSON in file: " ++ file
+            Just json -> case extractTitle json of
+                Nothing    -> putStrLn $ "No title found in file: " ++ file
+                Just title -> putStrLn $ "- " ++ unpack title
+
+extractTitle :: Value -> Maybe Text
+extractTitle = parseMaybe parseTitle
+
+parseTitle :: Value -> Parser Text
+parseTitle (Object v) = v .: "fullName"
+parseTitle _          = fail "Expected an object"
+
 
 displayHelp :: IO ()
 displayHelp =
